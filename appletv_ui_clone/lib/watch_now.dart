2d75a355b08da0ae24c7f2f62ff9056a57d94890
Glenn Inkoom <79399628+dwnlglendale@users.noreply.github.com>
2021-08-02 19:44:15 +0000

    second commit

--- appletv_ui_clone/lib/watch_now.dart
+++ appletv_ui_clone/lib/watch_now.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WatchNow extends StatefulWidget {
   const WatchNow({Key? key}) : super(key: key);
@@ -87,13 +88,120 @@ class _WatchNowState extends State<WatchNow> {
                 children: [
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    height: 250,
+                    height: 290,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(1),
                       color: Colors.white12,
                     ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Image.asset(
+                            "assets/movie2.jpg",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(13.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "Ted Lasso",
+                                        style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(
+                                        height: 0.5,
+                                      ),
+                                      Text(
+                                        "CONTINUE - S1,E4",
+                                        style: TextStyle(color: Colors.white60),
+                                      ),
+                                    ],
+                                  ),
+                                  Icon(
+                                    Icons.download_rounded,
+                                    color: Colors.blue,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 1.5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "What To Watch",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 170,
+                decoration: BoxDecoration(
+                  color: Colors.white12,
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Row(
+                      children: [Image(image: AssetImage("assets/movie1.jpg"))],
+                    ),
+                    SizedBox(
+                      width: 3.5,
+                    ),
+                    Row(
+                      children: [Image(image: AssetImage("assets/movie2.jpg"))],
+                    ),
+                    SizedBox(
+                      width: 3.5,
+                    ),
+                    Row(
+                      children: [Image(image: AssetImage("assets/movie3.jpg"))],
+                    ),
+                    SizedBox(
+                      width: 3.5,
+                    ),
+                    Row(
+                      children: [
+                        Image(image: AssetImage("assets/movie4.jpeg"))
+                      ],
+                    ),
+                    SizedBox(
+                      width: 3.5,
+                    ),
+                    Row(
+                      children: [Image(image: AssetImage("assets/movie5.jpg"))],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
