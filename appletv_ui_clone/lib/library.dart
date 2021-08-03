371ceeda9f0f522a7d5bdb00f81f0673a6b56392
Glenn Inkoom <79399628+dwnlglendale@users.noreply.github.com>
2021-08-03 10:34:30 +0000

    library view

--- appletv_ui_clone/lib/library.dart
+++ appletv_ui_clone/lib/library.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Library extends StatefulWidget {
@@ -10,9 +11,58 @@ class Library extends StatefulWidget {
 class _LibraryState extends State<Library> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Library"),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 40,
+              left: 15,
+            ),
+            child: Text(
+              "Library",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+
+          //Hardcoded to center (# need to change code).
+
+          SizedBox(
+            height: 300,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "No TV Shows or Movies",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Your library is empty. TV shows and movies you add to your library will appear here.",
+                  style: TextStyle(
+                    color: Colors.white38,
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
